6fc93ed4e943de8e68c649dc23379bb0452fe49e
Tiziano <tmartinelli@alumnos.unsada.edu.ar>
2024-11-06 11:39:45 -0300

    feat(install tailwindcss)

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LoginPage: React.FC = () => {
+    return (
+        <>
+            <div className="container-login-page flex justify-center items-center h-[100vh] bg-red-200">
+                asd
+            </div>
+        </>
+    );
+};
+
+export default LoginPage;

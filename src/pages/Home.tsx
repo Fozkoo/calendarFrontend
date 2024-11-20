da714f19326ff416658512f7805f19e4f4f40c20
Tiziano <tmartinelli@alumnos.unsada.edu.ar>
2024-11-20 13:36:57 -0300

    Feat (Home)
    -[x] Desing Calendar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,18 +8,20 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 
 function Home() {
   const [data, setData] = useState([]);
-  const [menuVisible, setMenuVisible] = useState(true); 
+  const [menuVisible, setMenuVisible] = useState(true);
 
 
   useEffect(() => {
     const fetchEvents = async () => {
       try {
-        const userId = "tiziano10";
+        const userId = "aaaaaa44444";
         const events = await servicesAPI.getAllEventsByIdUser(userId);
-        const formattedEvents = events.map((event: { title: any; day: any; }) => ({
-          title: event.title,
-          date: event.day,
+        const formattedEvents = events.map((event: { eventTitle: any; eventDay: any; }) => ({
+          title: event.eventTitle,
+          date: event.eventDay,  // Se usa eventDay directamente, ya que parece estar en el formato correcto
         }));
+        setData(formattedEvents);
+
         setData(formattedEvents);
       } catch (error) {
         console.error("Error fetching events:", error);
@@ -29,7 +31,7 @@ function Home() {
     fetchEvents();
   }, []);
 
-  
+
   const toggleMenu = () => {
     setMenuVisible(!menuVisible);
   };
@@ -38,22 +40,28 @@ function Home() {
 
   return (
     <>
-      <Header name="Tiziano" toggleMenu={toggleMenu}  /> 
+      <Header name="Tiziano" toggleMenu={toggleMenu} />
 
-      
+
 
       <div className={`container-page px-8 py-8 flex ${menuVisible ? 'justify-between' : 'justify-center'} items-center w-full h-[100vh]`}>
         <div className={`container-main-menu ${menuVisible ? 'flex' : 'hidden'} justify-center bg-slate-400 w-[23%] h-[100%] transition-all duration-300 ease-in-out`}>
           asdasd
         </div>
 
-        <div className={`container-calendar bg-red-600 flex  justify-center items-center ${menuVisible ? 'w-[75%]' : 'w-[90%]'} h-[100%] transition-all duration-300 ease-in-out`}>
+        <div className={`container-calendar   p-10   flex justify-center  ${menuVisible ? 'w-[75%]' : 'w-[90%]'} h-full transition-all duration-300 ease-in-out`}>
+
+          <div className="container-ccalendar flex    rounded-xl">
           <FullCalendar
-            plugins={[dayGridPlugin]}
-            initialView="dayGridMonth"
-            height={700}
-            events={data}/>
+              plugins={[dayGridPlugin]}
+              initialView="dayGridMonth"
+              events={data}
+              height="auto"
+            />
+          </div>
+
         </div>
+
       </div>
     </>
   );
